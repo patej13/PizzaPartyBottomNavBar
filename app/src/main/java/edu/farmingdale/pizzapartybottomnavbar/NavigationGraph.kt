c5cdcb1ae5450a6b69061a172976b253ca9fe4b5
Jay <patej13@farmingdale.edu>
2024-10-22 22:26:50 -0400

    Todo8

--- app/src/main/java/edu/farmingdale/pizzapartybottomnavbar/NavigationGraph.kt
+++ app/src/main/java/edu/farmingdale/pizzapartybottomnavbar/NavigationGraph.kt
@@ -1,9 +1,23 @@
 package edu.farmingdale.pizzapartybottomnavbar
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+
 @Composable
 fun NavigationGraph(navController: NavHostController, onBottomBarVisibilityChanged: (Boolean) -> Unit) {
     NavHost(navController, startDestination = BottomNavigationItems.Welcome.route) {
@@ -22,6 +36,47 @@ fun NavigationGraph(navController: NavHostController, onBottomBarVisibilityChang
         composable(BottomNavigationItems.Screen3.route) {
             onBottomBarVisibilityChanged(true)
             Screen3()
+        }
+    }
+}
+
+@Composable
+fun NavigationDrawer(
+    navController: NavHostController,
+    state: Boolean,
+    modifier: Modifier = Modifier
+) {
+    val screens = listOf(
+        BottomNavigationItems.PizzaScreen,
+        BottomNavigationItems.GpaAppScreen,
+        BottomNavigationItems.Screen3
+    )
+    NavigationBar(
+        modifier = modifier,
+        containerColor = Color.LightGray,
+    ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+
+        screens.forEach { screen ->
+            NavigationDrawerItem(
+                label = {
+                    Text(text = screen.title!!)
+                },
+                icon = {
+                    Icon(imageVector = screen.icon!!, contentDescription = "")
+                },
+                selected = currentRoute == screen.route,
+                onClick = {
+                    navController.navigate(screen.route) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                },
+            )
         }
     }
 }
